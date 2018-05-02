f64ad1acadaa83c46ef6da425024b1b4fa0ba1a9
= <=>
2018-05-01 20:40:00 -0600

    Extracted navbar bootstrap into a separate component for cleanliness

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA377xd3Y9z1RoGU680WIU1D9QGl_9yuso",
+    authDomain: "oshop-3ecdc.firebaseapp.com",
+    databaseURL: "https://oshop-3ecdc.firebaseio.com",
+    projectId: "oshop-3ecdc",
+    storageBucket: "",
+    messagingSenderId: "70392766113"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA377xd3Y9z1RoGU680WIU1D9QGl_9yuso",
+    authDomain: "oshop-3ecdc.firebaseapp.com",
+    databaseURL: "https://oshop-3ecdc.firebaseio.com",
+    projectId: "oshop-3ecdc",
+    storageBucket: "",
+    messagingSenderId: "70392766113"
+  }
 };
