7aed73baf5d26ad971801e9a8e69dbc5157555c2
= <=>
2018-05-06 10:10:05 -0600

    redirecting users from the login page to either the home page or the page the user is requesting.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,16 +2,20 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase'; // use for authentication instead of angular fire -- not as supported.
 import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable()
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth) { 
+  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) { 
     this.user$ = afAuth.authState; // use $ to easily read it's an observable.
   }
 
   login(){
+    let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
+    localStorage.setItem('returnUrl', returnUrl);
+    
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
