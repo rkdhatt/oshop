7eb403c86f7a121caf04f09c708724c742464b00
= <=>
2018-05-06 12:31:09 -0600

    Fixed bug where user could not log out. -- null observable returned from appUser$ method (auth.service.ts).

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,6 +6,8 @@ import * as firebase from 'firebase'; // use for authentication instead of angul
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/observable/of';
+
 
 @Injectable()
 export class AuthService {
@@ -30,6 +32,10 @@ export class AuthService {
   }
 
   get appUser$() : Observable<AppUser> {
-    return this.user$.switchMap(user => this.userService.get(user.uid))
+    return this.user$.switchMap(user => {
+      if (user) return this.userService.get(user.uid);
+
+      return Observable.of(null);
+    });
   }
 }
