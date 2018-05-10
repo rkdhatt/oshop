9b392b20fdd0116155c19e63a57f756636ef6bd8
= <=>
2018-05-09 20:14:36 -0600

    Populated category dropdown in product admin form using firebase.

---
+++ src/app/category.service.ts
@@ -0,0 +1,19 @@
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CategoryService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  getCategories()
+  {
+    return this.db.list('/categories', {
+      query: {
+        // can determine how to sort categories here.
+        // can find more info using angularfire docs on github.
+        orderByChild: 'name'
+      }
+    });
+  }
+}
