48a6f1b317428df6efe4fbe5142d6050f6a4dfb3
= <=>
2018-07-23 22:19:27 -0600

    Added filtering of products, but need to fix bug where page loads initally with no products (no filter applied)

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -8,12 +10,25 @@ import { ProductService } from '../product.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
-  products$;
+  products: Product[] = []; // to prevent error when refreshing.
+  filteredProducts: Product[];
   categories$;
+  category: string;
 
-  constructor(productService: ProductService, categoryService: CategoryService) { 
-    this.products$ = productService.getAll();
+  constructor(
+    route: ActivatedRoute,
+    productService: ProductService, 
+    categoryService: CategoryService) { 
+    productService.getAll().subscribe(products => this.products = products);
     this.categories$ = categoryService.getAll();
+
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      // apply filter
+      this.filteredProducts = (this.category) ? 
+        this.products.filter(p => p.category == this.category) : 
+        this.products;
+    });
   }
 
 }
